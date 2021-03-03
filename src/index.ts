9d3eea888e8b2ea6fad9b5ff011d6291fce2a8c7
David Yoon <davidyoon85@gmail.com>
2021-03-02 19:45:17 -0500

    extend LinkedList instead of Node class

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,18 @@ import { LinkedList } from "./LinkedList";
 // const collection = new NumbersCollection([-10, 9, -4, 6]);
 // const collection = new CharactersCollection("xYaZ");
 // const sorter = new Sorter(collection);
-// sorter.sort();
-// console.log(sorter.collection);
+// collection.sort();
+// console.log(collection);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
+// const charactersCollection = new CharactersCollection("aXbDja");
+// charactersCollection.sort();
+// console.log(charactersCollection);
 
-// sorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
